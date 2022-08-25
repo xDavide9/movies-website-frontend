6ba47dc6e5a852bf5bd4822e8cdd7c65c432582c
Dav <biancodavidetech@gmail.com>
2022-08-25 20:22:15 +0200

    implement all sliders

--- src/discover/DiscoverSliders.js
+++ src/discover/DiscoverSliders.js
@@ -12,16 +12,21 @@ import Slider from "react-slick";
 const { Text, Title } = Typography;
 
 const DiscoverSliders = (props) => {
-  const [results, setResults] = useState([]);
+  const [popularResults, setPopularResults] = useState([]);
+  const [year, setYear] = useState(2008);
+  const [yearResults, setYearResults] = useState([]);
+  const [genre, setGenre] = useState(28); // action
+  const [genreResults, setGenreResults] = useState([]);
 
   useEffect(() => {
+    // most popular films
     axios
       .get(
         `${process.env.REACT_APP_HTTP_REQUESTS_BASE}/.netlify/functions/api/discover/popular`
       )
       .then((res) => {
         console.log(res);
-        setResults(res.data.results);
+        setPopularResults(res.data.results);
       })
       .catch((err) => {
         notification.error({
@@ -34,7 +39,80 @@ const DiscoverSliders = (props) => {
         });
         console.log(err);
       });
-  });
+  }, []);
+
+  useEffect(() => {
+    // from year films
+    axios
+      .request({
+        url: `${process.env.REACT_APP_HTTP_REQUESTS_BASE}/.netlify/functions/api/discover/year`,
+        method: "GET",
+        params: {
+          year: year,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setYearResults(res.data.results);
+      })
+      .catch((err) => {
+        notification.error({
+          message: `Error ${err.response.status}`,
+          description: (
+            <>
+              {err.message} <br /> {err.response.data}
+            </>
+          ),
+        });
+        console.log(err);
+      });
+  }, [year]);
+
+  useEffect(() => {
+    // with genre films
+    // 28 action
+    // 12 adventure
+    // 16 animation
+    // 35 comedy
+    // 80 crime
+    // 99 documentary
+    // 18 drama
+    // 10751 family
+    // 14 fantasy
+    // 36 history
+    // 27 horror
+    // 10402 music
+    // 9648 mystery
+    // 10749 romance
+    // 878 science fiction
+    // 10770 tv movie
+    // 53 thriller
+    // 10752 war
+    // 37 western
+    axios
+      .request({
+        url: `${process.env.REACT_APP_HTTP_REQUESTS_BASE}/.netlify/functions/api/discover/genre`,
+        method: "GET",
+        params: {
+          genre: genre,
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setGenreResults(res.data.results);
+      })
+      .catch((err) => {
+        notification.error({
+          message: `Error ${err.response.status}`,
+          description: (
+            <>
+              {err.message} <br /> {err.response.data}
+            </>
+          ),
+        });
+        console.log(err);
+      });
+  }, [genre]);
 
   return (
     <div className={styles.topLevelContainer}>
@@ -43,7 +121,53 @@ const DiscoverSliders = (props) => {
       </Title>
       <div className={styles.sliderContainer}>
         <Slider {...settings}>
-          {results.map((result) => {
+          {popularResults.map((result) => {
+            return (
+              <Link to={`/discover/${result.id}/${props.language}`}>
+                <div className={styles.wrapper}>
+                  <img
+                    className={styles.poster}
+                    src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+                    alt="poster"
+                  />
+                  <Text className={`${styles.content} ${styles.fade}`}>
+                    Preview <EyeOutlined />
+                  </Text>
+                </div>
+              </Link>
+            );
+          })}
+        </Slider>
+      </div>
+      <Title level={3} className={styles.title}>
+        From Year {year}
+      </Title>
+      <div className={styles.sliderContainer}>
+        <Slider {...settings}>
+          {yearResults.map((result) => {
+            return (
+              <Link to={`/discover/${result.id}/${props.language}`}>
+                <div className={styles.wrapper}>
+                  <img
+                    className={styles.poster}
+                    src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+                    alt="poster"
+                  />
+                  <Text className={`${styles.content} ${styles.fade}`}>
+                    Preview <EyeOutlined />
+                  </Text>
+                </div>
+              </Link>
+            );
+          })}
+        </Slider>
+      </div>
+      <Title level={3} className={styles.title}>
+        With Genre {genre}
+      </Title>
+      <div className={styles.sliderContainer}>
+        <Slider {...settings}>
+          {genreResults.map((result) => {
             return (
               <Link to={`/discover/${result.id}/${props.language}`}>
                 <div className={styles.wrapper}>
